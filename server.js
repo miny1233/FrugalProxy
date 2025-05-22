8956a80e1124e6d1c0e70af0ec78c7781f3fd520
miny1233 <1596784863@qq.com>
2025-05-22 16:44:43 +0800

    服务端完成

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 const { Server } = require('socket.io');
+const net = require('net')
 
 console.log('正在启动服务端')
 
 const port = process.env.port || 80;
 
 const io = new Server({});
+const conn_map = new Map();
 
 io.on("connection", (socket) => {
-    console.log(`connect event ${socket}`)
+    console.log(`connect event: from ${socket.id}`)
+
+    socket.on("client-set-conn",(des_ip,des_port) => {
+        
+        const client = net.createConnection({
+            host: des_ip,
+            port: des_port,
+        },() => {    
+            console.log('成功连接到服务器')
+            conn_map[socket.id] = client;
+        });
+
+        client.on('error',()=> {
+            console.log('连接失败！ 正在通知客户端断开');
+
+            socket.emit('disconnect', '连接目标服务器失败');
+        })
+    })
+
+    socket.on("client-data",async (data) => {
+        
+        // 由于nodejs的单线程异步，那么当有定义时一定完成了连接
+        if(conn_map[socket.id] === undefined)
+        {   
+            await new Promise(timer => setTimeout(timer,50));
+        }
+        const client = conn_map[socket.id];
+        client.write(data);
+    })
+
+    socket.on("disconnect", (reson) => {
+        socket.disconnect();
+    })
 })
 
 io.listen(port);
