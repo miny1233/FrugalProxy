763533013197df1552bddc49f674c6fdba02ad16
miny1233 <1596784863@qq.com>
2025-05-22 15:54:24 +0800

    重新定义 protocol

--- protocol/data_struct.js
+++ protocol/data_struct.js
@@ -1,14 +1,45 @@
-class ConnectReq {
-    constructor(des_ip,des_port)
+/*
+    目前协议总共支持三种操作
+    连接，传输，断开
+*/
+
+const Method = {
+    Connect: 'connect',
+    Send: 'send',
+    Disconnect: 'disconnect'
+}
+
+/*
+    服务端和客户端是对等的，他们都是彼此的服务端
+    整个数据结构类似这样的
+    ++++++++++++++++++++++
+    + method + id + data +
+    ++++++++++++++++++++++
+
+    data会根据使用的method有所区别
+*/
+class DataControl {
+    constructor(method, id, data)
     {
-        this.des_ip = des_ip;
-        this.des_port = des_port;
+        this.method = method;
+        this.id = id;
+        this.data = data;
     }
 }
 
-class ConnectRes {
-    constructor(connection_id)
+/*
+    这里定义不同method方法下的data结构
+
+    连接:
+    +++++++++++++++++++++
+    + des_ip + des_port +
+    +++++++++++++++++++++
+*/
+
+class ConnectData {
+    constructor(des_ip, des_port)
     {
-        this.connection_id = connection_id;
+        this.des_ip = des_ip;
+        this.des_port = des_port;
     }
 }
